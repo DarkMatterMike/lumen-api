e19575d02e8858fdb984e7ead805ef730b118640
Lumen <dev@lumen.app>
2026-05-18 09:32:24 -0400

    add patch to cors

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const app = express()
 
 app.use(cors({
   origin: process.env.FRONTEND_URL || '*',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }))
 app.use(express.json())
